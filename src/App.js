6ae14b933585bfaa9f4b665cd1430e82b8cb11cc
Lucas Costa e Silva Almeida <lucas.almeida@compasso.com.br>
2022-05-17 15:54:58 -0300

    Aprendendo um pouco sobre React-Hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export default function App() {
+
+  const [repositories, setRepositories] = useState([]);
+  const [location, setLocation] = useState({});
+
+  useEffect(()=>{
+    navigator.geolocation.watchPosition(handlePosition)
+  },[])
+
+  const handlePosition = ({coords}) =>{
+    const {latitude, longitude} = coords;
+    setLocation({latitude,longitude})
+  }
+
+  useEffect( ()=>{
+    async function returnData(){
+      const response =  await fetch('https://api.github.com/users/lucas648/repos');
+      const data = await response.json();
+  
+      setRepositories(data);
+    }
+
+    returnData()
+  }, []);
+
+  useEffect(()=>{
+    const favorites = repositories.filter(repo=> repo.favorite);
+
+    document.title = `${favorites.length} repos favoritos!`
+  },[repositories]);
+
+  const handleFavorite = id =>{
+    const newRepositories = repositories.map(repo=>{
+      return repo.id === id ? {...repo, favorite: !repo.favorite} : repo;
+    })
+
+    setRepositories(newRepositories)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display: 'flex'}}>
+      <div>
+        <ul>
+          {
+            repositories.map(repository=>(
+              <li key={repository.id}>
+                {repository.name}
+                {repository.favorite && <span>(Favorito)</span>}
+                <button 
+                  onClick={()=>{handleFavorite(repository.id)}}
+                >
+                  {!repository.favorite ? 'Favoritar' : 'Desfavoritar'}
+                </button>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+      <div>
+        <div>
+          <h1>Latitude:</h1>
+          <label>{location.latitude}</label>
+        </div>
+        <div>
+          <h1>Longitude:</h1>
+          <label>{location.longitude}</label>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+
